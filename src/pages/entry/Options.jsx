bcd2551eaecab9e6f0e577e5fbf0b30cc98e49bc
Bartosz Ligęza <bartosz.ligeza@mlabs.pl>
2021-07-06 22:56:27 +0200

    error mocking

--- src/pages/entry/Options.jsx
+++ src/pages/entry/Options.jsx
@@ -1,18 +1,22 @@
 import axios from 'axios';
 import React, { useEffect, useState } from 'react';
 import { Row } from 'react-bootstrap';
+import AlertBanner from '../common/AlertBanner';
 import ScoopOption from './ScoopOption';
 import ToppingOption from './ToppingOption';
 
 const Options = ({ optionType }) => {
   const [items, setItems] = useState([]);
+  const [error, setError] = useState(false);
 
   // optionType is 'scoops' or 'toppings'
   useEffect(() => {
     axios
       .get(`http://localhost:3030/${optionType}`)
       .then((resp) => setItems(resp.data))
-      .catch((err) => console.log('err', err));
+      .catch((err) => {
+        setError(true);
+      });
   }, [optionType]);
 
   const ItemComponent = optionType === 'scoops' ? ScoopOption : ToppingOption;
@@ -24,6 +28,9 @@ const Options = ({ optionType }) => {
       imagePath={item.imagePath}
     />
   ));
+
+  // @ts-ignore
+  if (error) return <AlertBanner />;
 
   return <Row>{optionItems}</Row>;
 };

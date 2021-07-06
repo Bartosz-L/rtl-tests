18a66100fe7c075cd07f200a6083b96735881c09
Bartosz Ligęza <bartosz.ligeza@mlabs.pl>
2021-07-06 16:10:04 +0200

    initial

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
+const App = () => {
 
-function App() {
+  const [buttonColor, setButtonColor] = useState('red')
+  const [disabled, setDisabled] = useState(false)
+  const newButtonColor = buttonColor === 'red' ? 'blue' : 'red'
+
+  const handleClick = () => {
+    setButtonColor(newButtonColor)
+  }
+
+  const handleCheckboxClick = (e) => {
+    setDisabled(e.target.checked)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <button style={{ backgroundColor: disabled ? 'gray' : buttonColor }} onClick={handleClick} disabled={disabled}>Change to {newButtonColor}</button>
+
+      <input type="checkbox" id='disable-button-checkbox' defaultChecked={disabled} aria-checked={disabled} onChange={handleCheckboxClick} />
+      <label htmlFor="disable-button-checkbox">Disable button</label>
     </div>
   );
 }

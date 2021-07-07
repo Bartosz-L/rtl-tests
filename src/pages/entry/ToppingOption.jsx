42cc9aca58753aea7fece8a8f18401a6f9dd7941
Bartosz Ligęza <bartosz.ligeza@mlabs.pl>
2021-07-07 13:39:57 +0200

    styling

--- src/pages/entry/ToppingOption.jsx
+++ src/pages/entry/ToppingOption.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Col, Form, Row } from 'react-bootstrap';
+import { Col, Form } from 'react-bootstrap';
 
 const ToppingOption = ({ name, imagePath, updateItemCount }) => {
   const handleChange = (e) => {
@@ -7,7 +7,7 @@ const ToppingOption = ({ name, imagePath, updateItemCount }) => {
   };
 
   return (
-    <Col xs={12} sm={6} md={4} lg={3} style={{ textAlign: 'center' }}>
+    <Col xs={6} sm={4} md={3} lg={2} style={{ textAlign: 'center' }}>
       <img
         style={{ width: '75%' }}
         src={`http://localhost:3030/${imagePath}`}

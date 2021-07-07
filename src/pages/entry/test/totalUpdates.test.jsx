22a813cf3e6b620a64823f452b9a67d82db72c5b
Bartosz Ligęza <bartosz.ligeza@mlabs.pl>
2021-07-07 09:53:02 +0200

    created context

---
+++ src/pages/entry/test/totalUpdates.test.jsx
@@ -0,0 +1,32 @@
+import { screen, render } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import Options from '../Options';
+
+it('updates scoop subtotal when scoops change', async () => {
+  render(<Options optionType='scoops' />);
+
+  // make sure total start out $0.00
+  const scoopsSubtotal = screen.getByText('Scoops total: $', { exact: false });
+  expect(scoopsSubtotal).toHaveTextContent('0.00');
+
+  // update vanilla scoops to 1 and check the subtotal
+  const vanillaInput = await screen.findByRole('spinbutton', {
+    name: 'Vanilla',
+  });
+
+  userEvent.click(vanillaInput);
+  userEvent.type(vanillaInput, '1');
+
+  expect(scoopsSubtotal).toHaveTextContent('2.00');
+
+  // update chocolate scoops to 2 and check subtotal
+
+  const chocolateInput = await screen.findByRole('spinbutton', {
+    name: 'Chocolate',
+  });
+
+  userEvent.click(chocolateInput);
+  userEvent.type(chocolateInput, '2');
+
+  expect(scoopsSubtotal).toHaveTextContent('6.00');
+});

01761cf55752f2708de5a3d261ea83bf5019a9fb
Bartosz Ligęza <bartosz.ligeza@mlabs.pl>
2021-07-07 13:35:36 +0200

    order entry tests

--- src/pages/entry/OrderEntry.jsx
+++ src/pages/entry/OrderEntry.jsx
@@ -1,11 +1,16 @@
 import React from 'react';
+import { useOrderDetails } from '../../contexts/OrderDetails';
 import Options from './Options';
 
 const OrderEntry = () => {
+  const [orderDetails] = useOrderDetails();
+
   return (
     <div>
+      <h1>Design your Ice Creams</h1>
       <Options optionType='scoops' />
       <Options optionType='toppings' />
+      <h2>Grand total: {orderDetails.totals.grandTotal}</h2>
     </div>
   );
 };

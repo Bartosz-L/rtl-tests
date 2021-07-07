c7ef2dcc5110b8e3ea1f2a99b1d152dcd08ac8aa
Bartosz Ligęza <bartosz.ligeza@mlabs.pl>
2021-07-07 15:33:19 +0200

    scoop option test

--- src/pages/entry/test/Options.test.jsx
+++ src/pages/entry/test/Options.test.jsx
@@ -1,11 +1,12 @@
 import { render, screen } from '../../../test-utils/testing-library-utils';
+import userEvent from '@testing-library/user-event';
+
 import Options from '../Options';
 
-it('displays image for each scoop option from server', async () => {
+test('displays image for each scoop option from server', async () => {
   render(<Options optionType='scoops' />);
-  // find images
 
-  // when using async data use async function and findBy which is asynchronous
+  // find images
   const scoopImages = await screen.findAllByRole('img', { name: /scoop$/i });
   expect(scoopImages).toHaveLength(2);
 
@@ -15,23 +16,35 @@ it('displays image for each scoop option from server', async () => {
   expect(altText).toEqual(['Chocolate scoop', 'Vanilla scoop']);
 });
 
-it('displays image for each toppings option from server', async () => {
+test('Displays image for each toppings option from server', async () => {
+  // Mock Service Worker will return three toppings from server
   render(<Options optionType='toppings' />);
 
-  // find images
+  // find images, expect 3 based on what msw returns
+  const images = await screen.findAllByRole('img', { name: /topping$/i });
+  expect(images).toHaveLength(3);
 
-  // when using async data use async function and findBy which is asynchronous
-  const toppingsImages = await screen.findAllByRole('img', {
-    name: /topping$/i,
-  });
-  expect(toppingsImages).toHaveLength(3);
-
-  // confirm alt text of images
+  // check the actual alt text for the images
   // @ts-ignore
-  const altText = toppingsImages.map((element) => element.alt);
-  expect(altText).toEqual([
+  const imageTitles = images.map((img) => img.alt);
+  expect(imageTitles).toEqual([
     'Cherries topping',
     'M&Ms topping',
     'Hot fudge topping',
   ]);
+});
+
+test("don't update total if scoops input is invalid", async () => {
+  render(<Options optionType='scoops' />);
+
+  // expect button to be enabled after adding scoop
+  const vanillaInput = await screen.findByRole('spinbutton', {
+    name: 'Vanilla',
+  });
+  userEvent.clear(vanillaInput);
+  userEvent.type(vanillaInput, '-1');
+
+  // make sure scoops subtotal hasn't updated
+  const scoopsSubtotal = screen.getByText('Scoops total: $0.00');
+  expect(scoopsSubtotal).toBeInTheDocument();
 });
